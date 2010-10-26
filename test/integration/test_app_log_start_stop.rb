8d361c11e5f44f67a0cfdac8e897020e5e92e610
Eric Gjertsen <ericgj72@gmail.com>
2010-10-26 18:08:50 -0400

    integration tests now working (and passing); fixed server_runner_helper

--- test/integration/test_app_log_start_stop.rb
+++ test/integration/test_app_log_start_stop.rb
@@ -1,10 +1,55 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper.rb')
 require File.join(File.dirname(__FILE__), '..', 'shared', 'server_runner_helper.rb')
 require 'httparty'
+require 'json/pure'
 
-ServerRunnerHelper.run
+HOST = 'localhost'; PORT = '2003'
 
+class TestClient
+  include HTTParty
+  base_uri "#{HOST}:#{PORT}"
+end
+  
 describe 'start' do
- 
+  include ServerRunnerHelper
+  
+  before do
+    @project = 'test'
+    start_server
+  end
+  
+  after { stop_server }
+  
+  it 'should return new log' do
+    response = TestClient.post("/1/projects/#{@project}/logs")
+    response.body.wont_be_nil
+    response.body.wont_be_empty
+    log = JSON.parse(response.body)
+    puts log.inspect
+    log['project']['name'].must_equal @project
+  end
+  
+end
+
+describe 'start and stop' do
+  include ServerRunnerHelper
+  
+  before do
+    @project = 'test'
+    start_server
+    response = TestClient.post("/1/projects/#{@project}/logs")
+    log = JSON.parse(response.body)
+    @id = log['id']
+  end
+  
+  after { stop_server }
+  
+  it 'should return a duration ~2 secs' do
+    sleep 2
+    response = TestClient.put("/1/logs/#{@id}")
+    log = JSON.parse(response.body)
+    puts log.inspect
+    log['duration'].to_i.must_be_within_delta 2, 1
+  end
   
 end
--- test/shared/server_runner_helper.rb
+++ test/shared/server_runner_helper.rb
@@ -1,23 +1,47 @@
-# Note not sure subprocesses are the best route...
 
 module ServerRunnerHelper
   
-  def self.run
-    puts '-------------- server subprocess starting ------------------'
-    begin
-      svr = Kernel.open('| bin/trackd start -e test')
-      #line = ''
-      #while line = svr.gets
-      #  print line
-      #  break if line =~ /Listening on/
-      #end
-    ensure
-      svr.close if svr
+  def start_server
+    puts '-------------- server daemon starting ------------------'
+    `bin/trackd -d start -e test -l log/thin.log`
+    # monitor the log until server starts or error
+    th = tail_wait('log/thin.log', /^>>\s+Listening\s+on/)
+    th.join(15)
+    if block_given?
+      yield
+      stop_server
     end
   end
   
-#  def self.kill(pid)
-#    Process.kill("INT", pid)
-#  end
+  def stop_server
+    puts '-------------- server daemon stopping ------------------'
+    `bin/trackd -d stop`
+  end
   
+  private
+  
+    # Acts like GNU tail command. Adapted from Thin which took from Rails.
+    def tail_wait(file, pattern = nil)
+      cursor = File.exist?(file) ? File.size(file) : 0
+      last_checked = Time.now
+      tail_thread = Thread.new do
+        Thread.pass until File.exist?(file)
+        File.open(file, 'r') do |f|
+          loop do
+            f.seek cursor
+            if f.mtime > last_checked
+              last_checked = f.mtime
+              contents = f.read
+              cursor += contents.length
+              puts contents
+              break if pattern && contents =~ pattern || contents =~ /.rb:\d+:in\s/
+            end
+            sleep 0.1
+          end
+        end
+      end
+      sleep 1 if File.exist?(file) # HACK Give the thread a little time to open the file
+      tail_thread
+    end
+        
 end
