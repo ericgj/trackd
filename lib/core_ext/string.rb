02c37d77f627aa6d9eea5b11cd1b3743a3bbe7b9
Eric Gjertsen <ericgj72@gmail.com>
2010-11-15 13:24:42 -0500

    fix String#hhmm_to_seconds for negative times

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,8 +1,8 @@
 class String
 
   def hhmm_to_seconds
-    return unless self =~ /(\d+):(\d\d)/
-    $1.to_i*60*60 + $2.to_i*60
+    return unless self =~ /(-*)(\d+):(\d\d)/
+    ($1 == '-' ? -1 : 1) * ($2.to_i*60*60 + $3.to_i*60)
   end
   
 end
