35092f666807b08c869cccb6df6b8f3be8b8ceb2
ericgj <ericgj72@gmail.com>
2010-12-14 23:42:36 -0500

    started specs for changes to add/sub behavior (test_put_last_logs); add machinist blueprints

--- test/integration/test_app_log_start_stop.rb
+++ test/integration/test_app_log_start_stop.rb
@@ -1,7 +1,6 @@
-require File.join(File.dirname(__FILE__), '..', 'test_helper.rb')
-require File.join(File.dirname(__FILE__), '..', 'shared', 'server_runner_helper.rb')
 require 'httparty'
 require 'json/pure'
+require File.join(File.dirname(__FILE__), '..', 'test_helper.rb')
 
 HOST = 'localhost'; PORT = '2003'
 
